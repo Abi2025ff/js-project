b2b4b5e19bdfa2de37ac8cbbf7923c715310945f
abi <a86492188@gmail.com>
2025-12-07 21:38:50 +0500

    day2 functions

---
+++ src/day2.js
@@ -0,0 +1,13 @@
+function canDeploy(balance) {
+    return balance >= 2;
+}
+
+function getWalletStatus(address, balance) {
+    const status = canDeploy(balance) ? "READY" : "LOW_BALANCE";
+    return `Wallet ${address} has ${balance}ETH. Status: ${status}`;
+}
+
+const myBalance = 1.8;
+
+console.log("Can deploy" , canDeploy(myBalance));
+console.log(getWalletStatus("0x742d35Cc6634C0532925a3b8D4C9db4C2A8e7a12", myBalance));
