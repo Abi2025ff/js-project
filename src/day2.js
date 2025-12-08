ec5787c3e5d54fb0aa1eeaa0d147c89bcee2d3d5
abi <a86492188@gmail.com>
2025-12-08 16:20:29 +0500

    day2 backticks and function

--- src/day2.js
+++ src/day2.js
@@ -11,3 +11,6 @@ const myBalance = 1.8;
 
 console.log("Can deploy" , canDeploy(myBalance));
 console.log(getWalletStatus("0x742d35Cc6634C0532925a3b8D4C9db4C2A8e7a12", myBalance));
+
+// const name = "abi";
+// const greeting = `Hello, ${name}!`;
