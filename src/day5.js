2ec49c11bf1af700e5ff139021c18aa54fdb497c
abi <a86492188@gmail.com>
2025-12-11 16:44:03 +0500

    day5 conditions and logical operator

---
+++ src/day5.js
@@ -0,0 +1,20 @@
+const wallet = {
+   address: "0x635241",
+   balance: 5.2,
+   isActive: true,
+   role: "user"
+};
+
+if (wallet.balance >= 2 && wallet.isActive) {
+    console.log("Can deploy");
+}
+
+if (wallet.balance >= 5 || wallet.role === "admin") {
+    console.log("Account is premium");
+}
+
+if (wallet.role === "admin" || wallet.role === "moderator") {
+    console.log("Admin psnel access: GRANTED");
+} else {
+    console.log("Admin panel access: DENIED");
+}
