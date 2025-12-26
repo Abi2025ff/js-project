ce6b982336cd2c006f557e648ef323122e60caef
abi <a86492188@gmail.com>
2025-12-26 17:10:09 +0500

    getting the wallet balance from a node using alchemy, downloaded the ethers library

---
+++ src/day19.js
@@ -0,0 +1,13 @@
+const { ethers } = require("ethers");
+
+async function getBalance() {
+    const provider = new ethers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/OeWcKiQpGWROjv3F752zm");
+
+    const address = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e"; // Example wallet
+    const balanceWei = await provider.getBalance(address);
+
+    const balanceEth = ethers.formatEther(balanceWei);
+    console.log("Balance:", balanceEth, "ETH");
+}
+
+getBalance();
