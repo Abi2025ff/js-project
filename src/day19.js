f17726cc18e4328ef8c23f0b0e75861eb949d748
abi <a86492188@gmail.com>
2025-12-28 10:37:40 +0500

    I downloaded several libraries, moved the data files to a separate subfolder, removed my api key from the "day19.js" code, and learned how to work with .env

--- src/day19.js
+++ src/day19.js
@@ -1,7 +1,8 @@
 const { ethers } = require("ethers");
+require("dotenv").config({ path: "../.env" });
 
 async function getBalance() {
-    const provider = new ethers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/OeWcKiQpGWROjv3F752zm");
+    const provider = new ethers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);
 
     const address = "0x742d35Cc6634C0532925a3b844Bc454e4438f44e"; // Example wallet
     const balanceWei = await provider.getBalance(address);
@@ -10,4 +11,4 @@ async function getBalance() {
     console.log("Balance:", balanceEth, "ETH");
 }
 
-getBalance();
+getBalance()
